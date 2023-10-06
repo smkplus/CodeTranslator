486a52c46a28dbd199511e758c8724a45e9023bd
Seyed Morteza Kamali <smkplus3d@gmail.com>
2023-10-06 18:18:01 +0330

    fix bug

--- app.py
+++ app.py
@@ -44,7 +44,7 @@ def process_file():
 
 if __name__ == '__main__':
     # Use the PORT environment variable if provided, or default to 5000
-    port = int(os.environ.get('PORT', 5000))
+    port = int(os.environ.get('PORT', 8000))
 
     # Run the app with the specified host and port
     app.run(host='0.0.0.0', port=port)
